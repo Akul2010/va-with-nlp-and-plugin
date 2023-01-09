d5cbf9697def6e85f715d755ce5679f041713ad0
Akul Goel <akulgoel2010@gmail.com>
2023-01-09 18:34:14 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+import vosk
+import pyttsx3
+import importlib
+import os
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# Set up the speech recognition
+model = vosk.Model("path/to/model")
+recognizer = vosk.KaldiRecognizer(model, 16000)
+
+# Set up the self-learning neural network
+clf = MLPClassifier()
+
+# Load the plugins from the "plugins" directory
+plugins = []
+for filename in os.listdir("plugins"):
+  if filename.endswith(".py"):
+    module_name = filename[:-3]
+    plugin = importlib.import_module("plugins." + module_name)
+    plugins.append(plugin)
+
+# Define a list of commands that the voice assistant can recognize
+commands = ["hello", "what is your name", "how are you", "what time is it", "goodbye"]
+
+# Start the voice assistant loop
+while True:
+  # Listen for a command
+  print("Listening...")
+  audio = recognizer.AcceptAudio()
+
+  # Try to recognize the command
+  if audio.size > 0:
+    command = recognizer.Result()
+    print(f"You said: {command}")
+
+    # Check if the command matches any of the plugins
+    for plugin in plugins:
+      if plugin.is_valid_command(command):
+        plugin.handle_command(command, engine)
+        break
+    else:
+      # If the command is not handled by a plugin, try to classify it using the self-learning neural network
+      prediction = clf.predict(np.array(command).reshape(1, -1))
+      command_index = int(prediction[0])
+
+      # If the command is unknown, ask the user to label it
+      if command_index == len(commands):
+        engine.say("I'm sorry, I don't recognize that command. Could you please tell me what you meant by it?")
+        engine.runAndWait()
+        label = input("Enter the correct label for the command: ")
+        commands.append(label)
+        # Add the command and label to the training data
+        clf.partial_fit(np.array(command).reshape(1, -1), np.array(label).reshape(1, -1))
+      else:
+        # Perform the appropriate action based on the command
+        if commands[command_index] == "hello":
+          engine.say("Hello! How can I help you today?")
+          engine.runAndWait()
+        elif commands[command_index] == "what is your name":
+          engine.say("My name is Python Voice Assistant.")
+          engine.runAndWait()
+        # Add more elif blocks to handle other commands
+  else:
+    print("Sorry, I didn't hear anything.")
